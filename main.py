53e509f3039ec02016fcff83363f5f267c49102c
Joshua Abbey <Joshuaabbey2022@gmail.com>
2023-05-25 00:33:30 +0100

    added while loop so runs indefinitely

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import time
 import sys
 
 if __name__ == '__main__':
-    file_exist = os.path.isfile('tweets.csv')
-    if not file_exist:
-        collect.run_get_tweets()
-    else:
-        bot.tweet()
-        print("Waiting 1 hour")
-        for i in range(0, 3600):
-            sys.stdout.write(str(i)+'\r')
-            sys.stdout.flush()
-            time.sleep(1)
+    while True:
+        file_exist = os.path.isfile('tweets.csv')
+        if not file_exist:
+            collect.run_get_tweets()
+        else:
+            bot.tweet()
+            print("Waiting 1 hour")
+            for i in range(0, 3600):
+                sys.stdout.write(str(i)+'\r')
+                sys.stdout.flush()
+                time.sleep(1)
             
